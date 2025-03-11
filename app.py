054d6c64150f749bf66c47587524a9a69a656909
Parvez <rijwanoolkarim@gmail.com>
2025-03-11 20:19:53 +0530

    c

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def download_video():
     ydl_opts = {
         'format': format_id,
         'noplaylist': True,
-        'cookiefile': 'chrome',  # Extract cookies from Chrome browser
+        'cookiefile': 'youtube_cookies.txt',  # Extract cookies from Chrome browser
     }
 
     try:
